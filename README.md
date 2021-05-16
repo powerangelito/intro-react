89b26c27d74ef8b117b0e005364d316bbae408c9
powerangelito <angelito_dya@hotmail.com>
2021-05-16 16:36:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8mu5cf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8mu5cf)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+const App = () => {
+  return <h1> Hola mundo </h1>;
+};
+
+render(<App />, document.getElementById('react-app'));
