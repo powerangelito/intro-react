b22dc1f53e1e9953aef967b240b719cc878ce13e
powerangelito <angelito_dya@hotmail.com>
2021-05-16 16:53:05 -0500

    Condicionales

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// function SaludarEnIdiomas({ idioma }) {
+//   if (idioma === 'es') return <span>Hola</span>;
+//   if (idioma === 'en') return <span>Hello</span>;
+// }
+
 const Saludo = () => {
   const nombre = 'Angel';
-  return <p> Hola {`${nombre}`} </p>;
+  const idioma = 'es';
+  return (
+    <div>
+      {/* <SaludarEnIdiomas idioma="es" /> {`${nombre}`} */}
+      {/* {idioma === "en" ? <p>Hello</p> : <p>Hola</p>} */}
+      {idioma === 'es' && <p>Hola</p>}
+      {idioma === 'en' && <p>Hello</p>}
+    </div>
+  );
 };
 
 const App = () => {
