45b6937b329be4b1247bfe27ea941e7956733496
powerangelito <angelito_dya@hotmail.com>
2021-05-16 16:42:27 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+const Saludo = () => {
+  const nombre = 'Angel';
+  return <p> Hola {`${nombre}`} </p>;
+};
+
 const App = () => {
-  return <h1> Hola mundo </h1>;
+  return (
+    <h1>
+      <Saludo />
+    </h1>
+  );
 };
 
 render(<App />, document.getElementById('react-app'));
