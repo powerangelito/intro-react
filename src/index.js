cb756266c074b05cf3b7536893d4a8ad2b3e75b2
powerangelito <angelito_dya@hotmail.com>
2021-05-16 21:27:59 -0500

    evento

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 
-//Componente es de clase
-/**
- * componentDidMount()
- * componentidUpdate()
- * componentWillUnmount()
- */
-
-const Button = () => {
-  //Aquí no debe haber efectos secundarios
-  //hook
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log('Me ejecuté');
-  }, []);
-  return <button onClick={() => setCount(count + 1)} >click {count}</button>;
+const Saludo = () => {
+  const [name, setName] = useState('');
+  return (
+    /* SyntheticEvent */
+    <div>
+      <input type="text" onChange={ (ev) => setName(ev.target.value) }/>
+      <p>Hola {name}</p>
+    </div>
+  );
 };
 
 const App = () => {
   return (
     <div>
-      <Button />
+      <Saludo />
     </div>
   );
 };
