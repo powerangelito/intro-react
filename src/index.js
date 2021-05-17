f29dac68bcfb517135da8a722e1eec2a83e7f65d
powerangelito <angelito_dya@hotmail.com>
2021-05-16 20:10:56 -0500

    Definiendo componentes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const nombres = ['Angel', 'Elisa', 'Rebeca'];
+// const App = () => {
+//   return (
+//     <h1>Hola mundo</h1>
+//   );
+// }
 
-function getNombres() {
-  const elementosLista = [];
-  for (var i = 0; i < nombres.length; i++) {
-    elementosLista.push(<li>{nombres[i]}</li>);
+// const App = () => <h1>Hola mundo</h1>;
+
+class App extends Component {
+  render() {
+    return <h1>Hola mundo</h1>;
   }
-  return elementosLista;
 }
-
-const Nombres = () => {
-  // return <ul>{getNombres()}</ul>;
-  return (
-    <ul>
-      {nombres.map((nombre, index) => (
-        <li key={index}>{nombre}</li>
-      ))}
-    </ul>
-  );
-};
-
-const App = () => {
-  return (
-    <div>
-      <Nombres />
-    </div>
-  );
-};
 
 render(<App />, document.getElementById('react-app'));
