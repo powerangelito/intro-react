b40146e7d274847b8ff5b15f452d3c46cacd298f
powerangelito <angelito_dya@hotmail.com>
2021-05-16 21:39:53 -0500

    formulario react

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const Saludo = () => {
-  const [name, setName] = useState('');
+const Form = () => {
+  let [title, setTitle] = useState('');
+  let [body, setBody] = useState('');
+
   return (
-    /* SyntheticEvent */
-    <div>
-      <input type="text" onChange={ (ev) => setName(ev.target.value) }/>
-      <p>Hola {name}</p>
-    </div>
+    <form>
+      <div>
+        <label for="title">Título</label>
+        <input
+          type="text"
+          id="title"
+          onChange={ev => setTitle(ev.target.value)}
+        />
+      </div>
+      <div>
+        <label for="body">Publicación</label>
+        <textarea id="body" onChange={ev => setBody(ev.target.value)} />
+      </div>
+      <input type="submit" value="Enviar" />
+    </form>
   );
 };
 
 const App = () => {
   return (
     <div>
-      <Saludo />
+      <Form />
     </div>
   );
 };
